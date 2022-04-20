98ab2c9fc01911f4394f67930571734aeb300b8d
Matt Kumar <mattkumar@gmail.com>
2022-04-20 09:09:07 -0400

    Update get_hyperlink.py
    final version of the function

--- python/get_hyperlink.py
+++ python/get_hyperlink.py
@@ -1,20 +1,20 @@
 import openpyxl as xl
-import pandas   as pd
+import pandas as pd
 
 def get_hyperlink(path, sheet):
 
-  # Load in file, sheet
+  # Define workbook, worksheet
   wb = xl.load_workbook(path)
   ws = wb.get_sheet_by_name(sheet)
   
   # For all cells in the worksheet, if a hyperlink is detected, extract the hyperlink target 
-  # Otherwise just keep the original value
+  # Otherwise just keep the original value as a string
   for row_cells in ws.iter_rows():
     for cell in row_cells:
       try:
         cell.value = cell.hyperlink.target
       except:
-        cell.value = cell.value
+        cell.value = str(cell.value)
     
   # Store values to a data frame, clean up headers 
   tmp = pd.DataFrame(ws.values)
@@ -22,6 +22,7 @@ def get_hyperlink(path, sheet):
   tmp.drop(tmp.index[0], inplace = True)
   
   return tmp
+  
 
 
  
